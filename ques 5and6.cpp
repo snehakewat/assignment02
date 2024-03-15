23539085935d982a9354d0f97536cd70bcd45adb
snehakewat <159766627+snehakewat@users.noreply.github.com>
2024-03-15 17:36:44 +0530

    Add files via upload

---
+++ ques 5and6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main (){
+    char a,b;
+    cout<<"enter the characters"<<endl;
+    cin>>a;
+    cin>>b;
+    cout<<b-a;
+    return 0:
+} 
+
+
+
+
+
+
+
+ 
---
+++ ques4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int r;
+    int h;
+    cout<<"enter radius and height "<<endl;
+    cin>>r;
+    cin>>h;
+    int pi=3.14;
+    int volume;
+    volume=pi*r*r*h;
+    cout<<volume;
+    return 0;
+    
+}
